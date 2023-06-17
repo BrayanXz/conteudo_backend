03d6b8ce8c1c02cd0727fda23be03dc7a510f947
BrayanXz <125468888+BrayanXz@users.noreply.github.com>
2023-06-17 14:11:08 -0300

    Update cabeçalho.php

--- trabalho/template/cabeçalho.php
+++ trabalho/template/cabeçalho.php
@@ -25,7 +25,7 @@
       <img src="tutg.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="ttg(1).jpg" class="d-block w-100" alt="...">
+      <img src="ttg (1).jpg" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
